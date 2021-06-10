0efd60a4fae0c8a7b06560414103d414d0209537
wei <a1234567045@yahoo.com.tw>
2021-06-10 21:52:19 +0800

    feat: add facebook login & dotenv

--- routes/index.js
+++ routes/index.js
@@ -4,11 +4,14 @@ const router = express.Router()
 const home = require('./modules/home')
 const todos = require('./modules/todos')
 const users = require('./modules/users')
+const auth = require('./modules/auth')   // 引用模組
+
 const { authenticator } = require('../middleware/auth')  // 掛載 middleware
 
 
 router.use('/todos', authenticator, todos)
 router.use('/users', users)
+router.use('/auth', auth)  // 掛載模組
 router.use('/', authenticator, home)
 
 module.exports = router
